53f9ac9b434c359c285f93f9b066447018c4d751
coop_dev <cooperyan66@gmail.com>
2024-11-10 10:58:17 +0500

    add projects folder and add lesson #15

---
+++ lessons/n15-offline/main.js
@@ -0,0 +1,67 @@
+
+// const elements = {
+//   allInputs: document.querySelectorAll('input'),
+//   form: document.querySelector('form'),
+//   id: document.querySelector('.id'),
+//   firstName: document.querySelector('.firstName'),
+//   lastName: document.querySelector('.lastName'),
+//   email: document.querySelector('.email'),
+//   save: document.querySelector('.save'),
+//   load: document.querySelector('.load'),
+// }
+
+// const user = JSON.parse(localStorage.getItem('user'))
+
+// elements.allInputs.forEach((el) => {
+//   el.addEventListener('input', (e) => {
+
+//     user[e.target.className] = e.target.value
+
+//   })
+// })
+
+// elements.save.addEventListener('click', () => {
+//   localStorage.setItem('user', JSON.stringify(user))
+//   elements.form.reset();
+// })
+
+// elements.load.addEventListener('click', () => {
+//   const localUser = JSON.parse(localStorage.getItem('user'));
+
+//   elements.allInputs.forEach((el) => {
+//     el.value = localUser[el.className]
+//   })
+// })
+
+
+
+
+// const some = {}
+
+// some.user = 'its Im'
+
+
+// console.log(some);
+
+// const pretest = [1, 2, 3, 4, 5]
+// const pretest2 = [6, 7, 8, 9]
+// const test = [...pretest, ...pretest2]
+
+// const firstName = 'Ivan';
+// const firstname = 'dwadwa'
+
+// let user = {
+//   firstName,
+//   lastName: 'Tester'
+// }
+
+// user = {
+
+//   firstname,
+//   lastName: 'Und',
+//   ...user,
+// }
+
+// console.log(user);
+
+
