9348d3afdc1529802a0ff6c29b8b583a0d00bc11
22i01 <22i02samvong@gmail.com>
2024-12-03 17:05:12 +0500

    Add files via upload
    add hw-5
    
    Signed-off-by: 22i01 <22i02samvong@gmail.com>

---
+++ home-works/22i01/hw-5/main.js
@@ -0,0 +1,91 @@
+const lightStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=light]');
+const darkStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]');
+const darkSchemeMedia = matchMedia('(prefers-color-scheme: dark)');
+const switchersRadios = document.querySelectorAll('.switcher__radio');
+
+function setupSwitcher() {
+    const savedScheme = getSavedScheme();
+
+    if (savedScheme !== null) {
+        const currentRadio = document.querySelector(`.switcher__radio[value=${savedScheme}]`);
+        currentRadio.checked = true;
+        
+    }
+
+    [...switchersRadios].forEach((radio) => {
+        radio.addEventListener('change', (e) => {
+            setScheme(e.target.value)
+        });
+    });
+    
+}
+
+
+function setupScheme() {
+    const savedScheme = getSavedScheme();
+    const systemScheme = getSystemScheme();
+
+    if (savedScheme === null) return;
+
+    if (savedScheme !== systemScheme) {
+        setScheme(savedScheme);
+    }
+}
+
+
+function setScheme(scheme) {
+    switchMedia(scheme);
+
+    if (scheme === 'auto') {
+        clearScheme;
+        
+    } else {
+        savedScheme(scheme)
+        
+    }
+}
+
+function switchMedia (scheme) {
+    let lightMedia;
+    let darkMedia;
+
+    if (scheme === 'auto') {
+        lightMedia = '(prefers-color-scheme: light)';
+        darkMedia = '(prefers-color-scheme: dark)';
+    } else {
+        lightMedia = (scheme === 'light') ? 'all' : 'not all';
+        darkMedia = (scheme === 'dark') ? 'all' : 'not all';
+
+    }
+
+   [...lightStyles].forEach((link) => {
+    link.media = lightMedia;
+   });
+
+   [...darkStyles].forEach((link) => {
+    link.media = darkMedia;
+   });
+
+}
+
+
+function getSystemScheme() {
+    const darkScheme = darkSchemeMedia.matches;
+    
+    return darkScheme ? 'dark' : 'light';
+}
+
+function getSavedScheme() {
+    return localStorage.getItem('color-scheme');
+}
+
+function savedScheme(scheme) {
+    localStorage.setItem('color-scheme', scheme);
+}
+
+function clearScheme() {
+    localStorage.removeItem('color-scheme');
+}
+
+setupSwitcher();
+setupScheme();
