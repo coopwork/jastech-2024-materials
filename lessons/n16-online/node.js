affc967458d4761431f9eb472ede7d45408e4712
USERNAME NAME <Alex@mail.ru>
2024-12-20 18:48:38 +0500

    kontr 1

--- lessons/n16-online/node.js
+++
@@ -1,7 +0,0 @@
-
-
-console.log('hello world');
-
-
-console.log(2+2);
-
--- lessons/n26-online/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import { tabsManagement } from './tabs-manager.js';
-
-const tabBars = document.querySelectorAll('.tab__bar');
-
-tabBars.forEach(tabBar => {
-  tabBar.addEventListener('click', tabsManagement)
-});
