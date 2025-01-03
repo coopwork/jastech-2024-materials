8b26a6e32211e0f95b7c62ef9d6bd08019c972e3
Ruslan <cooperyan66@gmail.com>
2025-01-03 21:07:07 +0500

    add lesson #30

---
+++ lessons/n30-online/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css';
+import Button from './components/Button';
+import ShopItem from './components/ShopItem';
+import { shopItems } from './constants/shop-items';
+import Counter from './components/Counter';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <div className="App">
+      <h1>
+        WORKING
+      </h1>
+      <Counter/>
+      <Button 
+        onClick={()=> setIsLoading(true)} 
+        isLoading={isLoading}
+      >
+        Start app
+      </Button>
+
+      <Button
+        onClick={()=> setIsLoading(false)}
+      >
+        End app
+      </Button>
+
+      {shopItems?.map((shopItem)=>(
+        <ShopItem key={shopItem.id} item={shopItem}/>
+      ))}
+
+    </div>
+  );
+}
+
+export default App;
