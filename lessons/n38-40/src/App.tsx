7523c97e4b4bc4ebf82e3eba006bbd6336535120
coop_dev <cooperyan66@gmail.com>
2025-02-02 11:41:27 +0500

    add 38 lesson

---
+++ lessons/n38-40/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Button>Test</Button>
+    </>
+  )
+}
+
+export default App
