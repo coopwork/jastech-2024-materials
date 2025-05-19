98ced837b070f4fb9a1117a562d83f660cbd330a
22i01 <22i02samvong@gmail.com>
2025-05-19 16:01:18 +0500

    Delete hw-6 directory
    Signed-off-by: 22i01 <22i02samvong@gmail.com>

--- hw-6/speak.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.sp_btn').addEventListener('click', () => {
-    const text = document.querySelector('.text').value;
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = 'ru-RU';
-    speechSynthesis.speak(utterance);
-});
